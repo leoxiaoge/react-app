71768a693cadcd539021aa8dc1bd65bddb90b5ab
leoxiaoge <1269286430@qq.com>
2021-04-17 13:10:47 +0800

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="App-text">个人网站内容分享</p>
+        <a className="App-link" href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">粤ICP备18003568号</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
